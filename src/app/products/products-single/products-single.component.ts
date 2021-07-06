cfe4b05bfc00af22cacbea5e425326bf48c5ae34
Alex Yakushov <yakushov_as@groupbwt.com>
2021-07-06 17:54:51 +0300

    feat(product single): completed page

--- src/app/products/products-single/products-single.component.ts
+++ src/app/products/products-single/products-single.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {ProductsService} from "../../shared/services/products.service";
-import {IProduct} from "../../shared/interfaces/product";
-import {takeUntil} from "rxjs/operators";
+import {IProduct, IProductsPreview} from "../../shared/interfaces/product";
+import {tap} from "rxjs/operators";
 
 @Component({
   selector: 'app-products-single',
@@ -13,30 +13,47 @@ export class ProductsSingleComponent implements OnInit {
   public isLoading: boolean = false;
   public productId?: string;
   public product?: IProduct;
+  public productsPreviewData: IProductsPreview;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               private productsService: ProductsService) {
     this.productId = route.snapshot.params.id;
-  }
-
-  public getProductData(): void {
-    this.isLoading = true;
-    if (this.productId) {
-      this.productsService.getSingleProduct(this.productId)
-        .subscribe((product: IProduct) => {
-          if (product === null) {
-            this.router.navigate(['products']);
-          }
-          this.product = product;
-        }, () => {
-        }, () => this.isLoading = false)
-
+    this.productsPreviewData = {
+      title: 'Most Popular Products',
+      link: '/products',
+      buttonText: 'View All',
+      products: []
     }
   }
 
+  public getProductData(id: string): void {
+    this.isLoading = true;
+    this.productsService.getSingleProduct(id)
+      .subscribe((product: IProduct) => {
+        if (product === null) {
+          this.router.navigate(['products']);
+        }
+        this.product = product;
+      }, () => {
+      }, () => this.isLoading = false)
+  }
+
+  public initProductsList() {
+    const limit = 3;
+    this.productsService.getMostPopularProducts(limit).subscribe(
+      (products: IProduct[]) => {
+        this.productsPreviewData.products = products;
+      });
+  }
+
   ngOnInit(): void {
-    this.getProductData();
+    this.route.params.subscribe((params) => {
+      if (params.id !== null) {
+        this.getProductData(params.id);
+      }
+    })
+    this.initProductsList();
   }
 
 }
--- src/app/shared/interfaces/product.ts
+++ src/app/shared/interfaces/product.ts
@@ -1,6 +1,7 @@
 export interface IProduct {
   id: number,
   title: string,
+  description: string,
   image: string,
   category: string,
   price: string,
