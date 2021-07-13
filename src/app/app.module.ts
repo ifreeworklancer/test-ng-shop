87f36d4a6a89634c59d723e7d9461645883905f4
Alex Yakushov <yakushov_as@groupbwt.com>
2021-07-13 11:23:56 +0300

    feat(auth): added service and interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from '@angular/core';
-import {HttpClientModule} from "@angular/common/http";
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
 import {BrowserModule} from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -10,20 +10,21 @@ import {HeaderComponent} from './layout/header/header.component';
 import {FooterComponent} from './layout/footer/footer.component';
 import {HomeComponent} from './home/home.component';
 import {AppMaterialModule} from "./shared/app-material/app-material.module";
-import { NailComponent } from './components/banner/nail/nail.component';
-import { ProductsPreviewComponent } from './components/preview/products-preview/products-preview.component';
-import { ProductCardComponent } from './components/card/product-card/product-card.component';
-import { ProductsArchiveComponent } from './products/products-archive/products-archive.component';
-import { ProductsSingleComponent } from './products/products-single/products-single.component';
-import { ProductsFilterComponent } from './components/filter/products-filter/products-filter.component';
-import { ProductsCatalogComponent } from './components/catalog/products-catalog/products-catalog.component';
-import { FullscreenLoaderComponent } from './components/loader/fullscreen-loader/fullscreen-loader.component';
-import { BasketButtonComponent } from './components/basket/basket-button/basket-button.component';
-import { BasketComponent } from './basket/basket.component';
-import { BasketListComponent } from './components/basket/basket-list/basket-list.component';
-import { BasketItemComponent } from './components/basket/basket-item/basket-item.component';
-import { LoginComponent } from './auth/login/login.component';
-import { LoginFormComponent } from './components/form/auth/login-form/login-form.component';
+import {NailComponent} from './components/banner/nail/nail.component';
+import {ProductsPreviewComponent} from './components/preview/products-preview/products-preview.component';
+import {ProductCardComponent} from './components/card/product-card/product-card.component';
+import {ProductsArchiveComponent} from './products/products-archive/products-archive.component';
+import {ProductsSingleComponent} from './products/products-single/products-single.component';
+import {ProductsFilterComponent} from './components/filter/products-filter/products-filter.component';
+import {ProductsCatalogComponent} from './components/catalog/products-catalog/products-catalog.component';
+import {FullscreenLoaderComponent} from './components/loader/fullscreen-loader/fullscreen-loader.component';
+import {BasketButtonComponent} from './components/basket/basket-button/basket-button.component';
+import {BasketComponent} from './basket/basket.component';
+import {BasketListComponent} from './components/basket/basket-list/basket-list.component';
+import {BasketItemComponent} from './components/basket/basket-item/basket-item.component';
+import {LoginComponent} from './auth/login/login.component';
+import {LoginFormComponent} from './components/form/auth/login-form/login-form.component';
+import {JwtInterceptor} from "./shared/interceptors/jwt.interceptor";
 
 @NgModule({
   declarations: [
@@ -54,7 +55,9 @@ import { LoginFormComponent } from './components/form/auth/login-form/login-form
     AppMaterialModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true},
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
---
+++ src/app/shared/services/authentication.service.ts
@@ -0,0 +1,42 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../../environments/environment";
+import {map} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private currentUserSubject: BehaviorSubject<any>;
+  public currentUser: Observable<any>;
+
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<any>(JSON.parse(<string>localStorage.getItem('currentUser')));
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  get currentUserValue(): any {
+    return this.currentUserSubject.value;
+  }
+
+  get isLoginCurrentUser(): boolean {
+    return this.currentUserValue && Boolean(this.currentUserValue.token);
+  }
+
+  login(payload: object): any {
+    return this.http.post<any>(environment.apiUri.concat('auth/login'), payload)
+      .pipe(map(user => {
+        if (user && user.token) {
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+        }
+        return user;
+      }));
+  }
+
+  logout(): any {
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+  }
+}
