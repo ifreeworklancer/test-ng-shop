cff4fe99354d4b34379c9da49fda182ee7937d54
Alex Yakushov <yakushov_as@groupbwt.com>
2021-07-15 16:57:03 +0300

    fix: changed environment prod auth uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUri: 'https://fakestoreapi.com/',
-  authUri: 'https://fakestoreapi.com/',
+  authUri: 'https://reqres.in/api/',
   baseUri: 'http://localhost:4200/'
 };
